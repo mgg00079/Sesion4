df3dbd730efaf844bf195bf93147bea503d221a0
Marta <Marta@192.168.0.30>
2018-04-22 22:09:57 +0200

    Codigo actualizado

--- practica1_sesion4/practica1_sesion4-master/practica1_sesion4-master/practica1_sesion4/src/main/java/org/practica1/sesion4/DaoUsuarioInterface.java
+++
@@ -1,9 +0,0 @@
-package org.practica1.sesion4;
-
-import java.util.List;
-
-public interface DaoUsuarioInterface {
-	public void creaTabla();
-	public void insertarUsuario(DtoUsuario usuarios);
-	public List<DtoUsuario> leerUsuarios();
-}
